93e51d11cf39389f74da0bbf5dedd70ff246a011
Rick Lemmen <99640119+rick-appart@users.noreply.github.com>
2024-01-03 16:53:21 +0100

    Improve loadscreen spacing

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,11 @@
 <template>
   <!-- Intro -->
-  <div class="fixed w-screen h-screen inset-0 z-[100] bg-tertiary" id="intro">
+  <div class="fixed w-screen h-dvh inset-0 z-[100] bg-tertiary" id="intro">
     <div class="relative w-screen h-screen">
       <div class="absolute bottom-0 left-0 w-full h-full bg-tertiary">
         <div class="container h-full">
           <div class="absolute top-0 left-0 w-full h-2 bg-white timeline"></div>
-          <div class="flex flex-col-reverse gap-10 text-white h-full py-10">
+          <div class="flex flex-col-reverse gap-y-10 text-white h-full pb-20">
             <p id="intro-text" class="title--h1 text-white blur-0">Design</p>
             <p id="intro-text" class="title--h1 text-white blur-0">
               Development
