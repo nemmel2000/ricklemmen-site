f39a229130e58498987053b3c7c1df469842d1c8
Rick Lemmen <99640119+rick-appart@users.noreply.github.com>
2024-04-15 19:33:56 +0200

    Add vercel Speed insights

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,11 @@
 <template>
+  <SpeedInsights />
   <div class="relative bg-bg">
     <TheNavbar />
     <slot />
   </div>
 </template>
+
+<script setup>
+import { SpeedInsights } from "@vercel/speed-insights/nuxt";
+</script>
